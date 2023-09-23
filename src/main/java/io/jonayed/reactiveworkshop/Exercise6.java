3321213760ca3298e0f8faf2444f32a2f879059b
jonayed <jonayed153@gmail.com>
2023-09-23 20:42:32 +0600

    Set timeout

--- src/main/java/io/jonayed/reactiveworkshop/Exercise6.java
+++ src/main/java/io/jonayed/reactiveworkshop/Exercise6.java
@@ -1,6 +1,11 @@
 package io.jonayed.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
 
 public class Exercise6 {
 
@@ -8,13 +13,34 @@ public class Exercise6 {
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
+        Flux<String> unresponsiveFlux = ReactiveSources.unresponsiveFlux();
+        Mono<String> unresponsiveMono = ReactiveSources.unresponsiveMono();
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        // TODO: Write code here
+        String valueFromMono =
+                unresponsiveMono
+                        .timeout(Duration.ofSeconds(5L))
+                        .doOnError(
+                                e ->
+                                        System.out.println(
+                                                "Could not retrieve the value from unresponsiveMono within 5 seconds"))
+                        .onErrorReturn("")
+                        .block();
+        System.out.println(valueFromMono);
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
-        // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        List<String> valueFromFlux =
+                unresponsiveFlux
+                        .timeout(Duration.ofSeconds(5L))
+                        .doOnError(
+                                e ->
+                                        System.out.println(
+                                                "Could not retrieve the value from unresponsiveFlux within 5 seconds"))
+                        .onErrorReturn("")
+                        .collectList()
+                        .block();
+
+        System.out.println(valueFromFlux);
 
         System.out.println("Press a key to end");
         System.in.read();
