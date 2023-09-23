f357c7ced2b5294f172dcc90d6b8604934f41685
jonayed <jonayed153@gmail.com>
2023-09-23 17:29:58 +0600

    Exercise3: Blocking to get a list

--- src/main/java/io/jonayed/reactiveworkshop/Exercise3.java
+++ src/main/java/io/jonayed/reactiveworkshop/Exercise3.java
@@ -1,6 +1,7 @@
 package io.jonayed.reactiveworkshop;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Exercise3 {
 
@@ -10,11 +11,10 @@ public class Exercise3 {
 
         // Get all numbers in the ReactiveSources.intNumbersFlux stream
         // into a List and print the list and its size
-        // TODO: Write code here
+        List<Integer> integers = ReactiveSources.intNumbersFlux().toStream().toList();
+        System.out.println("List is " + integers);
+        System.out.print("List Size " + integers.size());
 
-
-        System.out.println("Press a key to end");
-        System.in.read();
     }
 
 }
